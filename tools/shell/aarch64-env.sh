cb52819b62ce6b316407cacd599812d1a50018e0
Takeharu KATO <tkato@elliptic-curve.net>
2018-06-25 02:29:31 +0900

    修正概要: asmoffset, gdbinitファイルの例, コンパイラパス設定スクリプトの例を追加.

---
+++ tools/shell/aarch64-env.sh
@@ -0,0 +1,5 @@
+export PATH=/opt/cross/aarch64-bare/current/bin:${PATH}
+export QEMU=/opt/qemu/aarch64/bin/qemu-system-aarch64
+export CROSS_COMPILE=/opt/cross/aarch64-bare/current/bin/aarch64-none-elf-
+export CPU=aarch64
+export HAL_DIR=raspi3
