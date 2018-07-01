47106dcff1355b80b01261864a5e22510f739826
Takeharu KATO <tkato@elliptic-curve.net>
2018-07-01 23:58:44 +0900

    Add a place holder of the x86-64 trapframe.

---
+++ include/hal-x86-64/exception.h
@@ -0,0 +1,13 @@
+/* -*- mode: c; coding:utf-8 -*- */
+/**********************************************************************/
+/*  OS kernel sample                                                  */
+/*  Copyright 2014 Takeharu KATO                                      */
+/*                                                                    */
+/*  X86-64 Exception definitions                                      */
+/*                                                                    */
+/**********************************************************************/
+#if !defined(_HAL_EXCEPTION_H)
+#define  _HAL_EXCEPTION_H
+typedef struct _exception_frame{
+}exception_frame;
+#endif  /*  _HAL_EXCEPTION_H   */
