b8bd1c219aaad05c616fbc3ed9a3c465ffe3b4d6
Takeharu KATO <tkato@elliptic-curve.net>
2018-07-04 01:05:06 +0900

    修正概要: block I/O 試作版。

--- include/kern/kern_types.h
+++ include/kern/kern_types.h
@@ -22,7 +22,8 @@ typedef int32_t          irq_prio;  /*< IRQ priority                 */
 typedef uint32_t         irq_attr;  /*< IRQ Attribute                */
 typedef uint8_t          precnt_t;  /*< Preemption counters          */
 typedef uint64_t       uptime_cnt;  /*< Jiffies                      */
-
 struct _thread;
+typedef obj_id_t           dev_id;  /*< Device ID                    */
+typedef obj_id_t           blk_no;  /*< Dick block number            */
 extern struct _thread *current;  /*< The pointer for the current thread  */
 #endif  /*  _KERN_KERN_TYPES_H  */
