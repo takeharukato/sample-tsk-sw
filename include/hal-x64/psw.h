0944fdb00be4047915f11d849d9d21e419ea9807
Takeharu KATO <tkato@elliptic-curve.net>
2018-07-18 03:30:05 +0900

    修正概要: アイドル時にCPUをhaltさせる処理を追加。

--- include/hal-x64/psw.h
+++ include/hal-x64/psw.h
@@ -54,14 +54,25 @@ typedef uint64_t psw_t;  /*< プロセサステータスワード  */
 			     "popfq\n\t"                \
 	   ::"m"(psw));					\
    }while(0)
+/*
+ * Note: Strictly speaking, there is a race condition between sti and hlt.
+ *       But this might not cause serious problem in typical cases.
+ */
+#define hal_suspend_cpu() do{			\
+		__asm__ __volatile__(		\
+			"sti\n\t"		\
+			"hlt\n\t"		\
+			: : :"memory");		\
+}while(0)
 #else  /*  CONFIG_HAL  */
 #define psw_enable_interrupt()  do{}while(0)
 #define psw_disable_interrupt() do{}while(0)
 #define barrier() do{}while(0)
 #define __save_psw(psw) do{}while(0)
 #define __restore_psw(psw) do{}while(0)
-#endif  /*  CONFIG_HAL  */
 #define hal_suspend_cpu() do{}while(0)
+#endif  /*  CONFIG_HAL  */
+
 void psw_disable_and_save_interrupt(psw_t *);
 void psw_restore_interrupt(psw_t *);
 #endif  /*  _KERN_PSW_H   */
