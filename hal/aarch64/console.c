fc7ccd4e73b21dff75448002a7bf186e0dc4be9c
Takeharu KATO <tkato@elliptic-curve.net>
2018-08-12 01:17:35 +0900

    修正概要: コンソールドライバ整理

--- hal/aarch64/console.c
+++ hal/aarch64/console.c
@@ -20,10 +20,6 @@ static device_driver console={
 	.close = NULL,	
 };
 
-static struct _aarch64_console_info{
-	wait_queue  waiters;
-}aarch64_console_info;
-
 off_t aarch64_console_read(inode *ip, file_descriptor *f, void *dst, off_t off, 
     size_t counts){
 	size_t remains;
@@ -55,39 +51,8 @@ off_t aarch64_console_write(inode *ip, file_descriptor *f, void *src, off_t off,
 	return (off_t)counts;
 }
 
-static int 
-aarch64_uart_handler(irq_no irq, struct _exception_frame *exc, void *private){
-	struct _aarch64_console_info *cfg = (struct _aarch64_console_info *)private;
-	uint32_t msk;
-	psw_t      psw;
-
-	msk = *UART_IMSC;
-	*UART_IMSC =  0x7FF;
-
-	psw_disable_and_save_interrupt(&psw);
-	if ( ( !( *UART_FR & 0x30 ) ) && ( !is_wque_empty(&cfg->waiters) ) ) 
-		wque_wakeup(&aarch64_console_info.waiters, WQUE_REASON_WAKEUP);
-	psw_restore_interrupt(&psw);
-
-	*UART_ICR = 0x7FF;    // clear interrupts
-
-	msk &= ~( ( 0x1 << 4) | ( 0x1 << 5 ) );
-
-	*UART_IMSC = msk;
-
-
-
-	return IRQ_HANDLED;
-}
-
 void 
 aarch64_console_init(void){
-	uint32_t msk;
-
-	wque_init_wait_queue(&aarch64_console_info.waiters);
-
-	*UART_IMSC = 0x7FF;
-	msk = *UART_IMSC;
 
 	*UART_ICR = 0x7FF;    // clear interrupts
 	*UART_IBRD = 2;       // 115200 baud
@@ -97,11 +62,5 @@ aarch64_console_init(void){
 	// enable Tx(0x100), Rx(0x200), and UART(0x001)
 	*UART_CR = (0x1 << 8) | (0x1 << 9) | (0x1 << 0) ; 
 
-	irq_register_handler(AARCH64_UART_IRQ, IRQ_ATTR_NESTABLE |IRQ_ATTR_EDGE, 
-	    1, &aarch64_console_info, aarch64_uart_handler);
-
-	register_device_driver(CONS_DEV, &console, &aarch64_console_info);
-
-	msk &= ~( ( 0x1 << 4) | ( 0x1 << 5 ) );
-	*UART_IMSC = msk;
+	register_device_driver(CONS_DEV, &console, NULL);
 }
